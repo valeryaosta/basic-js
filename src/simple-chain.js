70b188a1b337f6a4726097a56ac3b843073f2f37
valeryaosta <gerasimenko.valery@gmail.com>
2023-12-21 00:53:12 +0400

    feat: solve simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,28 +2,37 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    // проверка существует ли элемент в цепи с указанной позицией
+    // исп-ем this.chain[position - 1], т.к позиции в цепи начинаются с 1, а индексы массива - с 0
+    if(!this.chain[position - 1]){
+      this.chain = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
