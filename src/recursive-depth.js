96f73e8dab904d3966d397b2d6844ab763303088
valeryaosta <gerasimenko.valery@gmail.com>
2023-12-23 02:01:08 +0400

    feat: solve recursive-depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,9 +3,9 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class DepthCalculator with method calculateDepth
  * that calculates deoth of nested array
- * 
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,9 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    // нач. значение глубины = 1 (текущий уровень массива)
+    return arr.reduce((maxDepth, item) => {
+      // если элемент - массив, рекурсивно считаем его глубину и срав-аем с текущ. максим. глубиной
+      // если элемент - не массив, возвр-ем текущ. максим. значение глубины
+      return Array.isArray(item) ? Math.max(maxDepth, this.calculateDepth(item) + 1) : maxDepth;
+    }, 1);
   }
 }
 
