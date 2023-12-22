8f5e8b50557df94f53c9a2b4fde53773c77dc932
valeryaosta <gerasimenko.valery@gmail.com>
2023-12-23 02:14:03 +0400

    feat: solve mac-address task

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  // `^[0-9A-Fa-f]{2}` - наличие двух шестнадцатеричных цифр
+  // `(-[0-9A-Fa-f]{2}){5}` - наличие пяти групп, каждая из которых нач-ся с дефиса и содержит две шестнадцатеричные цифры
+  // `$` - конец строки
+  const regex = /^[0-9A-Fa-f]{2}(-[0-9A-Fa-f]{2}){5}$/;
+
+  return regex.test(n);
 }
 module.exports = {
   isMAC48Address
