3d96132892fa393a073b218352a24b0f306eb4e8
valeryaosta <gerasimenko.valery@gmail.com>
2023-12-24 02:16:01 +0400

    feat: solve delete-digit task, dns-stats task, encode-line task

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let maxNumber = 0;
+  const numberStr = n.toString();
+
+  for (let i = 0; i < numberStr.length; i++) {
+    let newNumberStr = numberStr.slice(0, i) + numberStr.slice(i + 1);
+    let newNumber = parseInt(newNumberStr, 10);
+    maxNumber = Math.max(maxNumber, newNumber);
+  }
+
+  return maxNumber;
 }
 
 module.exports = {
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let subdomain = '';
+
+    for (const part of parts) {
+      subdomain += '.' + part;
+      stats[subdomain] = (stats[subdomain] || 0) + 1;
+    }
+  }
+
+  return stats;
 }
 
 module.exports = {
