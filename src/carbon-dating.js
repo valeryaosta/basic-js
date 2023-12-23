844fec95634bea3d5dce1da02c75f2756517ddad
valeryaosta <gerasimenko.valery@gmail.com>
2023-12-24 00:58:51 +0400

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,32 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity)) {
+    return false;
+  }
+
+  const activity = parseFloat(sampleActivity);
+
+  if (activity <= 0 || activity > MODERN_ACTIVITY) {
+    return false;
+  }
+
+  // 0.693 —приближенное значение константы распада, вычисленное как 2(ln(2)). полураспад - уменьшение активности вдвое
+  const age = Math.log(MODERN_ACTIVITY / activity) / (0.693 / HALF_LIFE_PERIOD);
+
+  return age > 0 ? Math.ceil(age) : false;
 }
 
 module.exports = {
